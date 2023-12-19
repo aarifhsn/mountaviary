9214bcdbdd7aa3fecf0943ab56456335133b6d05
aarif <aarifhsn@gmail.com>
2023-12-19 19:41:00 +0600

    Updated

--- src/scripts.js
+++ src/scripts.js
@@ -1,10 +1,9 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const mobileMenuButton = document.getElementById('mobile-menu');
-    const navList = document.querySelector('.nav-list');
-  
-    mobileMenuButton.addEventListener('click', function () {
-      navList.classList.toggle('show');
-      mobileMenuButton.classList.toggle('active');
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  const mobileMenuButton = document.getElementById("mobile-menu");
+  const navList = document.querySelector(".nav-list");
+
+  mobileMenuButton.addEventListener("click", function () {
+    navList.classList.toggle("show");
+    mobileMenuButton.classList.toggle("active");
   });
-  
+});
